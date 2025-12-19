b9e538a5db1026639ed00677396e8bdd4c032fb3
Taryn <tmf24@njit.edu>
2025-12-18 22:32:36 -0500

    just to make sure

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { Router } from '@angular/router';
 import { FormsModule } from '@angular/forms';  // Import FormsModule
 import { RouterModule } from '@angular/router';
+import { HttpClient } from '@angular/common/http'; //import http client
 
 @Component({
   standalone: true,
@@ -13,8 +14,9 @@ import { RouterModule } from '@angular/router';
 export class LoginComponent {
   email: string = '';
   password: string = '';
+  token: string = '';
 
-  constructor(private router: Router) {}
+  constructor(private router: Router, private http: HttpClient) {}
 
   login() {
     console.log('Logging in with:', this.email, this.password);
@@ -24,12 +26,35 @@ export class LoginComponent {
       this.router.navigate(['/home']);  // Navigate to the homepage route
     } else {
       alert("Please enter both email and password!");
+      return;
     }
-  }
+    
+    this.http.post<any>('http://10.10.10.10:4001/login', {
+    	email: this.email,
+    	password: this.password,
+    	token: this.token //optional if using 2fa
+    }).subscribe({
+    	next: (res) => {
+    		console.log('Login response:', res);
+    		
+    		if (res.token) {
+    			localStorage.setItem('jwt', res.token);
+    			alert('Login Successful!')
+    			this.router.navigate(['/home']);
+    		} else {
+			alert('Login failed: ' + (res.error || 'Unknown error'));
+		}
+    	},
+    	error: (err) => {
+    		console.error('Login API error:', err);
+    		alert('Server error');
+    	}
+  });
+}
 
   goToSignup() {
     console.log("Navigate to signup page");
     this.router.navigate(['/signup']);
   }
 }
-
+	
